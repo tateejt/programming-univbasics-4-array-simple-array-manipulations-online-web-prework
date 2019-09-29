0a4ccc7263e11a787c2b2839e1f6839d84617f97
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:51:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,4 +43,5 @@ def using_insert(array, element)
   list_of_programming_languages.insert(3,another_language)
 end
   def using_uniq(array)
+    aircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
     
