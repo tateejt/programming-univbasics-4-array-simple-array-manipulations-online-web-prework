4939a3abf2ba022ab2b73d43e7b9b6fc1963e35b
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 01:12:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,7 +54,7 @@ end
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
-  print instructors
+  p instructors
 end
 
 def using_delete_at(array, integer)
