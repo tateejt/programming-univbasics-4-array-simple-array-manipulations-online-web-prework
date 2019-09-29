745cbc41c77f0b053c7485f59d758fd27a6b8c8c
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:52:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -44,4 +44,5 @@ def using_insert(array, element)
 end
   def using_uniq(array)
     haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-    aircuts
+    haircuts.uniq
+  end
