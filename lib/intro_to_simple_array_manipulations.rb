dd2bfceeaad40c8696528274f8a60a7c30d2a97b
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:37:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,5 +34,5 @@ def shift_with_args(array)
  def using_concat(array, array)
    my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
    more_favs = ["sports cars", "flatiron school"]
-   my_favorite_things.concat(more_favs)
+   my_favorite_things << more_favs
 end
