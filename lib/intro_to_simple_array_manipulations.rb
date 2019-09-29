4481774ea1fc843f015500fbee88d840faed0f2e
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:37:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,7 +31,7 @@ def shift_with_args(array)
    ice_cream_brands.shift(2)
  end
  
- def using_concat(array, array)
+ def using_concat(array)
    my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
    more_favs = ["sports cars", "flatiron school"]
    my_favorite_things << more_favs
