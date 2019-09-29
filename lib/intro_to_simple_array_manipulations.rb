5f6a9abc93aa51a2ed1fdd81dee6f84b83a5aa5b
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:26:42 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,38 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+end
+
+
+def pop_with_args(array)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    dog_breeds.pop(2)
+end 
+
+def using_shift(array)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  my_favorite_cities.shift
+end
+
+def shift_with_args(array)
+   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+   ice_cream_brands.shift(2)
+ end
+ 
+ def using_concat(array)
+   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs.concat(["sports cars", "flatiron school"])
+    my_favorite_things
+   end
