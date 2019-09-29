79e74200cbf559f42b5d9a6c2fef298113606fdf
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-29 00:50:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,6 +40,6 @@ end
 def using_insert(array, element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-  list_of_programming_languages.insert(another_language)
+  list_of_programming_languages.insert(3,another_language)
 end
   
